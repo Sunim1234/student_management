4f1c65427436c0e9eecf353496098c45a93efacf
Sunim1234 <163703346+Sunim1234@users.noreply.github.com>
2025-04-01 06:55:03 +0000

    Done

--- std_crud.py
+++ std_crud.py
@@ -1,3 +1,4 @@
+
 import psycopg2
 
 # connection to datbase and eexception handling
@@ -9,17 +10,14 @@ def db_connection():
     except Exception as e:
         print("Error connecting to database.")
         return None
-def create_tables():
+'''def create_tables():
     conn=db_connection()
     cursor=conn.cursor()
     cursor.execute("""
-        CREATE TABLE students (
-            student_id SERIAL PRIMARY KEY,
-            first_name VARCHAR(100) NOT NULL,
-            last_name VARCHAR(100) NOT NULL,
-            email VARCHAR(255) UNIQUE,
-            enrollment_date DATE DEFAULT CURRENT_DATE
-        );
+        CREATE TABLE IF NOT EXISTS teacher(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        age INT NOT NULL)
     """)
     conn.commit()
     cursor.close()
@@ -29,8 +27,45 @@ def create_tables():
 def insert_data(name,age):
     conn=db_connection()
     cursor=conn.cursor()
-    cursor.execute("INSERT INTO student (std_id,first_name) VALUES(%s,%s) RETURNING id",(name,age))
+    cursor.execute("INSERT INTO teacher (name,age) VALUES(%s,%s) RETURNING id",(name,age))
     conn.commit()
     cursor.close()
     conn.close()
-    print("Data inserted.")
+    print("Data inserted.")'''
+
+'''def upd_data(a):
+    conn=db_connection()
+    cursor=conn.cursor()
+    cursor.execute("UPDATE teacher SET age=56 WHERE age = '%s'",(a,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Data updated.")
+
+def del_data(a):
+    conn=db_connection()
+    cursor=conn.cursor()
+    cursor.execute("DELETE FROM teacher WHERE age = '%s'",(a,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Data deleted.")'''
+
+def show_table():
+    conn = db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM teacher;")
+    results = cursor.fetchall()
+    print('(id, name, age, course_id, department_id)')
+    for row in results:
+        print (row)
+    conn.commit()
+    cursor.close()
+
+# int/ void main equivalent
+if __name__ == "__main__":
+    '''create_tables()
+    insert_data("Ram",21)
+    upd_data(21);
+    del_data(21)'''
+    show_table()
